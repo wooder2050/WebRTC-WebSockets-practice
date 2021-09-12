6ec1f12ed993f3fa8c72910f84c950c92d655a6f
unknown <wooder2050@gmail.com>
2021-09-12 18:55:16 +0900

    imp: socket connected

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,19 @@
-hello;
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+function handleOpen() {
+  console.log('Connected to Server ✅');
+}
+
+socket.addEventListener('open', handleOpen);
+
+socket.addEventListener('message', (message) => {
+  console.log('New message : ', message.data);
+});
+
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server ❌');
+});
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 5000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http';
+import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -5,7 +7,38 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const ConvertBufferToStringFormat = (data) => {
+  const dataThatConvertedWithJsonStringify = JSON.stringify(data);
+  const { data: dataThatConvertedWithJsonParse } = JSON.parse(
+    dataThatConvertedWithJsonStringify
+  );
+  const bufferOriginal = Buffer.from(dataThatConvertedWithJsonParse);
+  const result = bufferOriginal.toString('utf8');
+  return result;
+};
+
+function onSocketClose() {
+  console.log('Disconnected from Browser ❌');
+}
+
+function onSocketMessage(message) {
+  const translatedMessageData = ConvertBufferToStringFormat(message);
+  console.log(translatedMessageData);
+}
+
+wss.on('connection', (socket) => {
+  console.log('Connected to Browser ✅');
+  socket.on('close', onSocketClose);
+  socket.on('message', onSocketMessage);
+  socket.send('hello!!');
+});
+
+server.listen(3000, handleListen);
